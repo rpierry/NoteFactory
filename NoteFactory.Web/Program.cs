26bab45e31f868349db013159be474bca9fc99ad
rpier <rpier@GREYAREA>
2022-12-28 15:54:35 -0800

    Add project files.

---
+++ NoteFactory.Web/Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+
+//app.MapGet("/", () => "Hello World!");
+
+app.Run();
