9525fb4a0423049ff3375138fd048b0bb30b6462
Rob <rpierry@gmail.com>
2022-12-30 14:48:02 -0800

    initial sequencer implementation

---
+++ NoteFactory.Web/scripts/Sequencer.ts
@@ -0,0 +1,95 @@
+﻿
+interface TimeSource {
+    currentTime: number;
+}
+
+enum NoteName {
+    A = 440,    
+}
+
+type Octave = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
+
+class Note {
+    beatDuration: number = 1;
+    noteName: NoteName = NoteName.A;
+    octave: Octave = 4;
+
+    getFrequency() {
+        return this.octave >= 4 ?
+                this.noteName * (2 << (this.octave - 4)) :
+                this.noteName / (2 << (4 - this.octave));
+    }
+
+}
+
+class Sequencer {
+    private readonly _timeSource;
+    private readonly _noteHandler;
+    constructor(timeSource: TimeSource, noteHandler: (startTime: number, notes: Note[]) => void) {
+        this._timeSource = timeSource;
+        this._noteHandler = noteHandler;
+    }
+
+    private _steps: number = 8;
+
+    get steps() {
+        return this._steps;
+    }
+
+    set steps(stepCount: number) {
+        this._steps = stepCount;
+        //setup _pattern
+    }
+
+    private _pattern = [ true, false, true, false, false, true, true, false ];
+
+    private _bpm: number = 120;
+    get bpm() {
+        return this._bpm;
+    }
+    set bpm(beatsPerMin: number) {
+        this._bpm = beatsPerMin;
+    }
+
+    getSecondsPerBeat() {
+        return 60 / this._bpm; 
+    }
+
+    private _playing = false;
+    private _timerId: number;
+    private readonly _timerCallback = 50;
+    private readonly _scheduleAheadSeconds = 0.1;
+    private _nextStepTime = 0;
+    private _currentStep = 0;
+
+    play() {
+        if (this._playing) return;
+        this._playing = true;        
+
+        this._currentStep = 0;
+        this._nextStepTime = this._timeSource.currentTime;
+        this.scheduleNotes();
+    }
+
+    private scheduleNotes() {        
+        while (this._nextStepTime < this._timeSource.currentTime + this._scheduleAheadSeconds) {
+            if (this._pattern[this._currentStep]) {
+                this._noteHandler(this._nextStepTime, [new Note()]);
+            }
+
+            this._nextStepTime += this.getSecondsPerBeat();
+            this._currentStep = (this._currentStep + 1) % this._steps;
+        }
+        this._timerId = setTimeout(this.scheduleNotes.bind(this), this._timerCallback);
+    }
+
+    stop() {
+        if (!this._playing) return;
+        this._playing = false;
+        console.log("Sequencer stopping!");
+
+        clearTimeout(this._timerId);
+    }
+}
+
+export { Sequencer, Note, TimeSource, NoteName, Octave };
--- NoteFactory.Web/scripts/app.ts
+++ NoteFactory.Web/scripts/app.ts
@@ -1,15 +1,18 @@
 ﻿
 import { ScheduledNote } from "./ScheduledNote.js";
+import { Sequencer, Note, TimeSource, NoteName, Octave } from "./Sequencer.js";
 
 const btnPlay: HTMLButtonElement = document.querySelector("#play");
 const btnStop: HTMLButtonElement = document.querySelector("#stop");
 const txtFreq: HTMLInputElement = document.querySelector("#freq");
 const rngVol: HTMLInputElement = document.querySelector("#vol");
+const rngBpm: HTMLInputElement = document.querySelector("#bpm");
 
 btnPlay.addEventListener("click", Play);
 btnStop.addEventListener("click", Stop);
 txtFreq.addEventListener("change", ChangeFrequency, false);
 rngVol.addEventListener("change", ChangeVolume, false);
+rngBpm.addEventListener("change", ChangeBpm, false);
 
 const ctx = new AudioContext();
 
@@ -18,37 +21,35 @@ gainNode.connect(ctx.destination);
 ChangeVolume();
 ChangeFrequency();
 
+const sequencer = new Sequencer(ctx, ScheduleNotes);
+
 let playing = false;
-let timerId: number;
 
 function Play() {
     if (playing) return;
     playing = true;
 
-    ScheduleNote();    
+    sequencer.play();
 }
 
-function ScheduleNote() {
-    let n =
-        new ScheduledNote(
-            parseFloat(txtFreq.value),
-            ctx.currentTime + 0.5,
-            0.5,
-            ctx,
-            gainNode,
-            () => {
-                console.log("done playing!");
-                //playing = false;
-            });
-
-    timerId = setTimeout(ScheduleNote, 1000);
+function ScheduleNotes(startTime: number, notes: Note[]) {
+    for (let n of notes) {
+        let sn =
+            new ScheduledNote(
+                n.noteName,
+                startTime,
+                n.beatDuration * sequencer.getSecondsPerBeat() * 0.9,
+                ctx,
+                gainNode,
+                () => { });
+    }
 }
 
 function Stop() {
     if (!playing) return;
     playing = false;
-
-    clearTimeout(timerId);
+    
+    sequencer.stop();    
 }
 
 function ChangeFrequency() {
@@ -57,4 +58,8 @@ function ChangeFrequency() {
 
 function ChangeVolume() {
     gainNode.gain.value = parseFloat(rngVol.value);
+}
+
+function ChangeBpm() {
+    sequencer.bpm = parseInt(rngBpm.value);
 }
