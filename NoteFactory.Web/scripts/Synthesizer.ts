e153a0fa22c8c0e483c0982303725f0a5c1cf6f5
Rob <rpierry@gmail.com>
2022-12-31 20:04:00 -0800

    initial synth implementation

---
+++ NoteFactory.Web/scripts/Synthesizer.ts
@@ -0,0 +1,67 @@
+﻿
+class Synthesizer {
+    private _ctx: AudioContext;
+    private _outputGain: GainNode;
+
+    constructor(ctx: AudioContext) {
+        this._ctx = ctx;
+        this._outputGain = this._ctx.createGain();
+        this._outputGain.connect(this._ctx.destination);
+    }
+
+    private throwIfNotPercentage(name: string, val: number) {
+        if ((val < 0) || (val > 1)) throw new Error("Invalid " + name + " - must be between 0 and 1 inclusive");
+    }
+
+    get gain() {
+        return this._outputGain.gain.value;
+    }
+
+    set gain(gain: number) {
+        this.throwIfNotPercentage("gain", gain);        
+
+        this._outputGain.gain.value = gain;
+    }
+
+    private _oscillatorType: OscillatorType = "sine";
+    get oscillatorType() {
+        return this._oscillatorType;
+    }
+
+    set oscillatorType(val: OscillatorType) {
+        this._oscillatorType = val;
+    }
+
+    private _releaseAfterPercent: number = 0.9;
+    get releaseAfterPercent() {
+        return this._releaseAfterPercent;
+    }
+
+    set releaseAfterPercent(val: number) {
+        this.throwIfNotPercentage("releaseAfterPercent", val);
+
+        this._releaseAfterPercent = val;
+    }
+
+    play(frequency: number, startTime: number, durationSeconds: number) {
+        let osc = this.createOscillator(frequency, this._outputGain);
+        osc.start(startTime);
+        osc.stop(startTime + (durationSeconds * this._releaseAfterPercent));
+    }
+
+    private createOscillator(frequency: number, destination: AudioNode): AudioScheduledSourceNode {
+        let osc = this._ctx.createOscillator();
+        osc.type = this._oscillatorType;       
+        osc.frequency.value = frequency;
+        osc.addEventListener("ended", this.oscEnded);
+
+        osc.connect(destination);
+        return osc;
+    }
+
+    private oscEnded(this: OscillatorNode, ev: Event): any {
+        this.disconnect();
+    }
+}
+
+export { Synthesizer };
--- NoteFactory.Web/scripts/app.ts
+++ NoteFactory.Web/scripts/app.ts
@@ -1,30 +1,23 @@
 ﻿
-import { ScheduledNote } from "./ScheduledNote.js";
 import { Sequencer, TimeSource } from "./Sequencer.js";
 import { Note, NoteName, Octave } from "./Note.js";
+import { Synthesizer } from "./Synthesizer.js";
 
 const btnPlay: HTMLButtonElement = document.querySelector("#play");
 const btnStop: HTMLButtonElement = document.querySelector("#stop");
 const btnToggle: HTMLButtonElement = document.querySelector("#toggle");
-const btnTestMap: HTMLButtonElement = document.querySelector("#testMap");
-const txtFreq: HTMLInputElement = document.querySelector("#freq");
 const rngVol: HTMLInputElement = document.querySelector("#vol");
 const rngBpm: HTMLInputElement = document.querySelector("#bpm");
+const rngOct: HTMLInputElement = document.querySelector("#octave");
 
 btnPlay.addEventListener("click", Play);
 btnStop.addEventListener("click", Stop);
 btnToggle.addEventListener("click", Toggle);
-btnTestMap.addEventListener("click", TestMap);
-txtFreq.addEventListener("change", ChangeFrequency, false);
 rngVol.addEventListener("change", ChangeVolume, false);
 rngBpm.addEventListener("change", ChangeBpm, false);
+rngOct.addEventListener("change", ChangeOctave, false);
 
 const ctx = new AudioContext();
-
-const gainNode = ctx.createGain();
-gainNode.connect(ctx.destination);
-ChangeVolume();
-ChangeFrequency();
 
 const sequencer = new Sequencer(ctx, ScheduleNotes);
 sequencer.addToStep(0, new Note(NoteName.A, 4, 1));
@@ -34,6 +27,9 @@ sequencer.addToStep(3, new Note(NoteName.D, 4, 1));
 sequencer.addToStep(4, new Note(NoteName.E, 4, 1));
 sequencer.addToStep(5, new Note(NoteName.F, 4, 1));
 sequencer.addToStep(6, new Note(NoteName.G, 4, 1));
+
+const synthesizer = new Synthesizer(ctx);
+ChangeVolume();
 
 let playing = false;
 
@@ -46,14 +42,7 @@ function Play() {
 
 function ScheduleNotes(startTime: number, notes: Note[]) {
     for (let n of notes) {
-        let sn =
-            new ScheduledNote(
-                n.getFrequency(),
-                startTime,
-                n.beatDuration * sequencer.getSecondsPerBeat() * 0.9,
-                ctx,
-                gainNode,
-                () => { });
+        synthesizer.play(n.getFrequency(), startTime, n.beatDuration * sequencer.getSecondsPerBeat());        
     }
 }
 
@@ -64,16 +53,17 @@ function Stop() {
     sequencer.stop();    
 }
 
-function ChangeFrequency() {
-    //oscNode.frequency.value = parseFloat(txtFreq.value);
-}
-
-function ChangeVolume() {
-    gainNode.gain.value = parseFloat(rngVol.value);
+function ChangeVolume() {    
+    synthesizer.gain = parseFloat(rngVol.value);
 }
 
 function ChangeBpm() {
     sequencer.bpm = parseInt(rngBpm.value);
+}
+
+function ChangeOctave() {
+    sequencer.mapNotes(
+        (n: Note) => new Note(n.noteName, parseInt(rngOct.value) as Octave, n.beatDuration));
 }
 
 let incl = false;
@@ -85,13 +75,4 @@ function Toggle() {
         sequencer.addToStep(0, n);
     }
     incl = !incl;
-}
-
-let up = true;
-function TestMap() {
-    let mod = up ? 1 : -1;
-
-    sequencer.mapNotes((n: Note) => new Note(n.noteName, (n.octave + mod) as Octave, n.beatDuration));
-
-    up = !up;
 }
