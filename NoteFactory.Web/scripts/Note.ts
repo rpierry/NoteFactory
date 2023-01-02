3a7b44205fa7167ec50487412cd7f853fcbc8c42
Rob <rpierry@gmail.com>
2023-01-02 09:17:33 -0800

    added frequencies for the rest of the notes

--- NoteFactory.Web/scripts/Note.ts
+++ NoteFactory.Web/scripts/Note.ts
@@ -1,13 +1,18 @@
 ﻿import { SameAs } from "./Sequencer.js";
 
 const NoteNames = {
-    C: 261.63,
-    D: 293.67,
-    E: 329.63,
-    F: 349.23,
-    G: 392,
-    A: 440,
-    B: 493.88
+    C:  261.63,
+    Db: 277.18,
+    D:  293.67,
+    Eb: 311.13,
+    E:  329.63,
+    F:  349.23,
+    Gb: 369.99,
+    G:  392,
+    Ab: 415.30,
+    A:  440,
+    Bb: 466.16,
+    B:  493.88
 } as const;
 
 type NoteName = typeof NoteNames[keyof typeof NoteNames];
