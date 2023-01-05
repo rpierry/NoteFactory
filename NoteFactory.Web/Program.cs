83df5bc8d37f07b4fc7c07222d5da44cb5ce26ee
Rob <rpierry@gmail.com>
2023-01-04 16:54:06 -0800

    basic htmx

--- NoteFactory.Web/Program.cs
+++ NoteFactory.Web/Program.cs
@@ -6,5 +6,6 @@ app.UseStaticFiles();
 
 
 //app.MapGet("/", () => "Hello World!");
+app.MapGet("/createSession", () => "Hello from the <em>server</em><ol><li>One</li><li>Two</li></ol>");
 
 app.Run();
