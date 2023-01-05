e0f185174086acdddd658447e2e07366c990a479
Rob <rpierry@gmail.com>
2023-01-04 19:59:57 -0800

    basic get/post workflow scaffolding for starting shared jams

---
+++ NoteFactory.Web/Controllers/JamsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace NoteFactory.Web.Controllers
+{
+    public class JamsController : Controller
+    {
+        public IActionResult Create()
+        {
+            var id = "awefawef";
+            return RedirectToAction(nameof(Current), new { id });
+        }
+
+        public IActionResult Current(string id)
+        {            
+            return View(new { id });
+        }
+
+        public IActionResult Disconnect(string id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- NoteFactory.Web/Program.cs
+++ NoteFactory.Web/Program.cs
@@ -1,11 +1,56 @@
+using Microsoft.AspNetCore.Routing.Constraints;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+
+if(!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    //app.UseHsts();
+}
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+app.MapControllerRoute(
+    "jams",
+    "Jams/Disconnect/{id}",
+    new
+    {
+        controller = "Jams",
+        action = "Disconnect"
+    },
+    new { id = @"\w+", httpMethod = new HttpMethodRouteConstraint(HttpMethods.Post) });
 
-//app.MapGet("/", () => "Hello World!");
-app.MapGet("/createSession", () => "Hello from the <em>server</em><ol><li>One</li><li>Two</li></ol>");
+app.MapControllerRoute(
+    "jams",
+    "Jams/Current/{id}",
+    new
+    {
+        controller = "Jams",
+        action = "Current"
+    },
+    new { id = @"\w+" });
+
+app.MapControllerRoute(
+    "jams",
+    "Jams/Index",
+    new
+    {
+        controller = "Jams",
+        action = "Index"
+    });
+
+app.MapControllerRoute(
+    "jams",
+    "Jams/Create",
+    new 
+    {
+        controller = "Jams",
+        action = "Create"
+    },
+    new { httpMethod = new HttpMethodRouteConstraint(HttpMethods.Post) });
 
 app.Run();
