d2e0730a9b60e95c202da553447b5fd1dbd18248
Rob <rpierry@gmail.com>
2023-01-12 15:45:42 -0800

    scroll messages to end upon new message

--- NoteFactory.Web/Program.cs
+++ NoteFactory.Web/Program.cs
@@ -3,7 +3,6 @@ using NoteFactory.Web;
 using NoteFactory.Web.Hubs;
 
 //TODO: visual design
-//TODO: scroll messages to end on new message
 //TODO: undo/revert button after loading someone else's shared grid?
 
 var builder = WebApplication.CreateBuilder(args);
